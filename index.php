e61199c99923b4b579cbd7195155a637254fb228
VictorHGH <64684570+VictorHGH@users.noreply.github.com>
2026-06-08 09:52:02 -0600

    [Updated] main index.php

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-<?php
-	echo "<h1>Hola mundo desde master_php</h1>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Master_php</title>
+</head>
+
+<body>
+	<h1>Hola mundo desde master_php</h1>
+</body>
+
+<style>
+body {
+	background-color: black;
+	color: white;
+}
+h1 {
+	text-align: center;
+}
+</style>
+
+</html>
